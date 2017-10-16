af7ae594ef0eb0239fb0bdebb1845aebea1fcc8b
あまみや ゆうこ <1@1234.sh>
2017-10-16 23:27:28 +0800

    Implement summary api

--- src/JoyOI.Forum/Controllers/HomeController.cs
+++ src/JoyOI.Forum/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JoyOI.Forum.Models;
+using JoyOI.UserCenter.SDK;
 
 namespace JoyOI.Forum.Controllers
 {
@@ -11,6 +14,46 @@ namespace JoyOI.Forum.Controllers
         public IActionResult Index()
         {
             return RedirectToAction("Index", "Forum");
+        }
+
+        public async Task<IActionResult> Summary(
+            [FromServices] JoyOIUC UC,
+            System.Threading.CancellationToken token)
+        {
+            var threads = await DB.Threads
+                .Include(x => x.Forum)
+                .Include(x => x.User)
+                .Include(x => x.Posts)
+                .Where(x => x.IsAnnouncement)
+                .ToListAsync(token);
+
+            IEnumerable<Thread> plainThreads = new List<Thread>();
+
+            if (threads.Count < 10)
+            {
+                var count = 10 - threads.Count;
+                plainThreads = await DB.Threads
+                    .Include(x => x.Forum)
+                    .Include(x => x.User)
+                    .Include(x => x.Posts)
+                    .Where(x => !x.IsAnnouncement)
+                    .OrderByDescending(x => x.LastReplyTime)
+                    .Take(count)
+                    .ToListAsync(token);
+            }
+
+            var raw = threads.Concat(plainThreads);
+            return Json(raw.Select(x => new {
+                forumTitle =  x.Forum.Title,
+                forumId = x.ForumId,
+                threadId = x.Id,
+                threadTitle = x.Title,
+                time = x.LastReplyTime,
+                userId = x.User.OpenId,
+                username = x.User.UserName,
+                avatarUrl = UC.GetAvatarUrl(x.User.OpenId),
+                replyCount = x.Posts.Count
+            }));
         }
     }
 }
